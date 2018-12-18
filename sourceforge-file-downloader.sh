297bd48eaeeea46124d74d0f8261df4a067fb58d
Ross Smith II <ross@smithii.com>
2018-12-18 15:33:20 -0800

    Allow re-running without redownloading
    Force SourceForge to serve .zip files by setting user agent to Wget/1.19.1
    Download each file once, by removing duplicates in urllist
    Require all URLs to start with https: (to avoid URL encoded URLs)
    Don't re-download files that have already been downloaded

--- sourceforge-file-downloader.sh
+++ sourceforge-file-downloader.sh
@@ -16,15 +16,17 @@ echo "Downloading $project's files"
 
 # download all the pages on which direct download links are
 # be nice, sleep a second
-wget -w 1 -np -m -A download http://sourceforge.net/projects/$project/files/
+wget -w 1 -np -m -A download --no-clobber http://sourceforge.net/projects/$project/files/
 
 # extract those links
-grep -Rh refresh sourceforge.net/ | grep -o "https[^\\?]*" > urllist
+grep -Rh refresh sourceforge.net/ | grep -o "https:[^\\?]*" | sort -u > urllist
+
+printf "Downloading %d URLs\n" "$(wc -l < urllist)"
 
 # remove temporary files, unless you want to keep them for some reason
-rm -r sourceforge.net/
+# rm -r sourceforge.net/
 
 # download each of the extracted URLs, put into $projectname/
-while read url; do wget --content-disposition -x -nH --cut-dirs=1 "${url}"; done < urllist
+while read url; do wget -U Wget/1.19.1 --content-disposition -x -nH --cut-dirs=1 "${url}"; done < urllist
 
-rm urllist
+# rm urllist
